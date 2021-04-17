531e170fe8f424d982ddbd524beac26addb5ad09
Kalyan Sunkara <ksunk001@wch136-24.cs.ucr.edu>
2021-04-17 01:11:40 -0700

    saving first exercise

--- source/main.c
+++ source/main.c
@@ -11,13 +11,58 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
+enum LED_States {LED_SMStart, LED_B0on, LED_B1on} LED_State;
+void SMTick(){
+	switch(LED_State){
+		case LED_SMStart:
+			LED_State = LED_B0on;
+			break;
+		case LED_B0on:
+			if(PINA & 0x01){
+				LED_State = LED_B1on;
+			}
+			else{
+				LED_State = LED_B0on;
+			}
+			break;
+	 	case LED_B1on:
+			if(PINA & 0x01){
+                                LED_State = LED_B0on;
+                        }
+                        else{
+                                LED_State = LED_B1on;
+                        }
+                        break;     
+		default:
+			LED_State = LED_SMStart;
+			break;
+	}
+	switch(LED_State) {   // State actions
+     		case LED_SMStart:
+        		break;
+		case LED_B0on:
+			PORTB = 0x01;
+			break;
+		case LED_B1on:
+			PORTB = 0x01;
+                        break;
+		default:
+			break;
+		}
 
+
+}
 int main(void) {
     /* Insert DDR and PORT initializations */
+	DDRA = 0x00;
+	PORTA = 0xFF;
 
+	DDRB = 0xFF;
+	PORTB = 0x00;
     /* Insert your solution below */
+	LED_State = LED_SMStart;
     while (1) {
-
+	SMTick();
     }
     return 1;
 }
