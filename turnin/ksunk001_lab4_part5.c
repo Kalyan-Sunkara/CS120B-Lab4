73749df9f989f7ea0097bf7393db7d7269099734
Kalyan Sunkara <ksunk001@wch136-24.cs.ucr.edu>
2021-04-18 16:25:36 -0700

    Completed part 5

---
+++ turnin/ksunk001_lab4_part5.c
@@ -0,0 +1,97 @@
+/*	Author: ksunk001
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+enum Lock_States {Lock_SMStart, Lock_code, LOCK, UNLOCK, Lock_wait} Lock_State;
+unsigned char lockArray[4] = {0x04, 0x01, 0x02, 0x01};
+unsigned char count = 0;
+void SMTick(){
+	switch(Lock_State){
+		case Lock_SMStart:
+			Lock_State = Lock_code;
+			break;
+		case Lock_code:
+			if(PINA & 0x80){
+                                Lock_State = LOCK;
+                        }
+			else if(count == 4){
+				count = 0;
+				Lock_State = UNLOCK;
+			}
+			else if(PINA == 0x00){
+				Lock_State = Lock_code;
+			}
+			else if(PINA == lockArray[count]){
+				count++;
+				Lock_State = Lock_wait;
+			}
+			else{
+				Lock_State = Lock_code;
+				count = 0;	
+			}
+			break;
+		case Lock_wait:
+			if(PINA == 0){
+				Lock_State = Lock_code;
+			}
+			else{
+				Lock_State = Lock_wait;
+			}
+			break;
+		case UNLOCK:
+			Lock_State = Lock_code;
+			break;
+		case LOCK:
+			Lock_State = Lock_code;
+			break;
+		default:
+			Lock_State = Lock_SMStart;
+			break;
+	}
+	switch(Lock_State) {   // State actions
+ 		case Lock_SMStart:
+			break;
+		case Lock_wait:
+			break;
+		case Lock_code:
+			break;
+		case UNLOCK:
+			PORTB = !PORTB; //flips if locked or unlocked 
+			break;
+		case LOCK:
+			PORTB = 0x00;
+			break;
+		default:
+			break;
+		}
+
+
+}
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00;
+	PORTA = 0xFF;
+	
+	DDRB = 0xFF;
+	PORTB = 0x00;
+
+	DDRC = 0xFF;
+	PORTC = 0x00;
+    /* Insert your solution below */
+//	LED_State = LED_SMStart;
+	//Counter_State = Counter_start;
+    while (1) {
+	SMTick();
+	PORTC = Lock_State;
+    }
+    return 1;
+}
